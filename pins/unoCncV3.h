4c2a8983058b62841f2293b1ac2021861853b621
truglodite <imelman@msn.com>
2024-04-03 21:09:48 -0700

    fixed cnc shield enable pins

--- pins/unoCncV3.h
+++ pins/unoCncV3.h
@@ -1,13 +1,16 @@
 // Truglodite: rename to match CNC v3 shield https://osoyoo.com/wp-content/uploads/2017/04/Arduino-CNC-Shield-Scematics-V3.XX_.jpg
 #define eleStep      2   // X step
 #define eleDir       5   // X dir
+#define eleEN        8   // One pin used for all steppers
 #define aziStep      3   // Y Step
 #define aziDir       6   // Y Dir
+#define aziEN        8   // One pin used for all steppers
 #define polStep      4   // Z Step
 #define polDir       7   // Z Dir
+#define polEN        8   // One pin used for all steppers
 #define tunStep      12  // A Step (open spindle en jumper)
 #define tunDir       13  // A Dir (open spindle dir jumper)
-#define aziMinStop   8   // All Steppers Enable
+#define tunEN        8   // One pin used for all steppers
 // No boards with hardware pullups so far
 #define aziMinStop   9   // X endstop
 #define eleMinStop   10  // Y endstop
