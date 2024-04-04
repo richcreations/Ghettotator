45662ab77d7f2dbe8b116f176717f980b956e5f3
richcreations <richcreations@gmail.com>
2024-04-04 11:03:56 -0700

    Add files via upload

---
+++ boards/ramps_V_1_4.h
@@ -0,0 +1,28 @@
+// Pins file for Ramps v1.4... who doesn't have a spare one laying around these days?
+// https://osoyoo.com/wp-content/uploads/2016/07/Schematic-2.png
+// https://osoyoo.com/wp-content/uploads/2016/07/Schematic-1.png
+#define eleStep      A0  // X step
+#define eleDir       A1  // X direction
+#define eleEN        38  // X enable
+#define aziStep      A6  // Y step
+#define aziDir       A7  // Y direction
+#define aziEN        A2  // Y enable
+#define polStep      46  // Z step
+#define polDir       48  // Z direction
+#define polEN        A8  // Z enable
+#define tunStep      26  // E0 step
+#define tunDir       28  // E0 direction
+#define tunEN        24  // E0 enable
+#define auxStep      36  // E1 step
+#define auxDir       34  // E1 direction
+#define auxEN        30  // E1 enable
+// Code uses internal pullups
+#define eleMinStop   3   // X- endstop
+#define eleMaxStop   2   // X+ endstop
+#define aziMinStop   14  // Y- endstop
+#define aziMaxStop   15  // Y+ endstop
+#define polMinStop   18  // Z- endstop
+#define polMaxStop   19  // Z+ endstop
+
+#define ledExists    1  // onboard led
+#define ledPin       13  // onboard led
