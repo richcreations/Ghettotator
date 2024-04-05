095b4c5e02a33d52058886e0de04d9d0fe7f4e62
richcreations <richcreations@gmail.com>
2024-04-04 22:57:20 -0700

    defaults mostly fleshed out

--- src/config.h
+++ src/config.h
@@ -7,40 +7,40 @@
 
 #define POLARIZER              // Uncomment to enable antenna polarity rotator feature (pot wired between 0V and 5V)
 
-#define BAUDRATE           9600  // Set the Baudrate of easycomm 3 protocol (9600)
-
-#define AZI_RATIO          57.0  // Azimuth mechanical gear ratio
-#define AZI_MICROSTEP      16.0  // Azimuth stepper driver microstepping
-#define AZI_VMAX           300   // Antenna deg/s
-#define AZI_ACC_MAX        120   // Antenna deg/s^2
-#define AZI_MIN_ANGLE      0     // Minimum antenna azimuth angle
-#define AZI_MAX_ANGLE      360   // Maximum antenna azimuth angle
-
-#define ELE_RATIO          57.0  // Elevation mechanical gear ratio
-#define ELE_MICROSTEP      16.0  // Elevation stepper driver microstepping
-#define ELE_VMAX           300   // Antenna deg/s
-#define ELE_ACC_MAX        120   // Antenna deg/s^2
-#define ELE_MIN_ANGLE      0     // Minimum antenna elevation angle
-#define ELE_MAX_ANGLE      180   // Maximum antenna elevation angle
-
-#define POL_RATIO          57.0  // Polarization mechanical gear ratio
-#define POL_MICROSTEP      16.0  // Polarization stepper driver microstepping (built in MSx pull ups)
-#define POL_VMAX           300   // Antenna deg/s
-#define POL_ACC_MAX        120   // Antenna deg/s^2
-#define POL_MIN_ANGLE      0     // Minimum antenna polarization angle
-#define POL_MAX_ANGLE      180.0 // Maximum antenna polarization angle
-
-#define SPR                200.0 // Motor steps per revolution (200.0 for standard 1.8degree motors)
-#define MIN_PULSE_WIDTH    20    // uSec (for AccelStepper)
-#define DEFAULT_HOME_STATE LOW   // Low for N.O. switches and pullups
-#define HOME_DELAY         0     // millis to keep moving after hitting endstop
-#define SAMPLE_TIME        0.1   // Control loop in sec
-
-#define WATCHDOG                 // Disable motors if WDT is triggered
 //#define DEBUG                  // Uncomment and move debug led to troubled spot 
 
+#define WATCHDOG                 // Disable motors if WDT is triggered
 
-// Do not modify below this line! -------------------------------------------------------------------
 
+// The following lines can be uncommented and vales changed to overide default settings (default settings shown for reference)
+
+// #define BAUDRATE           9600  // Set the Baudrate of easycomm 3 protocol (9600)
+
+// #define AZI_RATIO          57.0  // Azimuth mechanical gear ratio
+// #define AZI_MICROSTEP      16.0  // Azimuth stepper driver microstepping
+// #define AZI_VMAX           300   // Antenna deg/s
+// #define AZI_ACC_MAX        120   // Antenna deg/s^2
+// #define AZI_MIN_ANGLE      0     // Minimum antenna azimuth angle
+// #define AZI_MAX_ANGLE      360   // Maximum antenna azimuth angle
+
+// #define ELE_RATIO          57.0  // Elevation mechanical gear ratio
+// #define ELE_MICROSTEP      16.0  // Elevation stepper driver microstepping
+// #define ELE_VMAX           300   // Antenna deg/s
+// #define ELE_ACC_MAX        120   // Antenna deg/s^2
+// #define ELE_MIN_ANGLE      0     // Minimum antenna elevation angle
+// #define ELE_MAX_ANGLE      180   // Maximum antenna elevation angle
+
+// #define POL_RATIO          57.0  // Polarization mechanical gear ratio
+// #define POL_MICROSTEP      16.0  // Polarization stepper driver microstepping (built in MSx pull ups)
+// #define POL_VMAX           300   // Antenna deg/s
+// #define POL_ACC_MAX        120   // Antenna deg/s^2
+// #define POL_MIN_ANGLE      0     // Minimum antenna polarization angle
+// #define POL_MAX_ANGLE      180.0 // Maximum antenna polarization angle
+
+// #define SPR                200.0 // Motor steps per revolution (200.0 for standard 1.8degree motors)
+// #define MIN_PULSE_WIDTH    20    // uSec (for AccelStepper)
+// #define DEFAULT_HOME_STATE LOW   // Low for N.O. switches and pullups
+// #define HOME_DELAY         0     // millis to keep moving after hitting endstop
+// #define SAMPLE_TIME        0.1   // Control loop in sec
 
 
